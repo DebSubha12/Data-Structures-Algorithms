f20b18a8ccbd8f9bfa5e69aa841e254c530e4663
Subhajit Ghosh <subhajit2004jit@gmail.com>
2026-05-09 21:07:02 +0530

    Linked List Cycle(Leetcode #141)

---
+++ Circular_Linklist/LinkListCycle.java
@@ -0,0 +1,51 @@
+package Circular_Linklist;
+
+public class LinkListCycle {
+       static class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+     public boolean hasCycle(Node head) {
+
+        Node slow = head;
+        Node fast = head;
+
+        while (fast != null && fast.next != null) {
+
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+public static void main(String[] args) {
+    Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(4);
+
+         // Creating cycle
+        head.next.next.next.next = head.next;
+
+        // Object of class
+        LinkListCycle obj = new LinkListCycle();
+
+        // Checking cycle
+        boolean ans = obj.hasCycle(head);
+
+        if (ans) {
+            System.out.println("Cycle Detected");
+        } else {
+            System.out.println("No Cycle Found");
+        }
+}
+}
